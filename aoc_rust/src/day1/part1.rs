e50a6ce0abf0399770146c3eaffb761e2111e7fa
Swarnim Arun <swarnimarun11@gmail.com>
2025-12-02 02:28:52 +0530

    feat: day 1 part 2 and cleanup

--- aoc_rust/src/day1/part1.rs
+++ aoc_rust/src/day1/part1.rs
@@ -1,20 +1,12 @@
 pub fn run(input: String) {
-    let mut result = 0;
-    _ = input
+    let mut acc = 50;
+    let res = input
         .lines()
-        .map(|s| {
-            if s.as_bytes()[0] == b'L' {
-                s[1..].parse::<i64>().unwrap() * -1
-            } else {
-                s[1..].parse::<i64>().unwrap()
-            }
+        .map(|s| s[1..].parse::<i64>().unwrap() * if s.as_bytes()[0] == b'L' { -1 } else { 1 })
+        .filter(|res| {
+            acc = ((acc + res) % 100 + 100) % 100;
+            acc == 0
         })
-        .fold(50i64, |acc, res| {
-            let r = ((acc + res) % 100 + 100) % 100;
-            if r == 0 {
-                result += 1;
-            }
-            r
-        });
-    println!("Result for the day is: {}", result);
+        .count();
+    println!("Result for the day is: {}", res);
 }
--- aoc_rust/src/day1/part2.rs
+++ aoc_rust/src/day1/part2.rs
@@ -1,1 +1,13 @@
-pub fn run(_input: String) {}
+pub fn run(input: String) {
+    let mut previous = 50;
+    let mut rotations = 0;
+    input
+        .lines()
+        .map(|s| s[1..].parse::<i64>().unwrap() * if s.as_bytes()[0] == b'L' { -1 } else { 1 })
+        .for_each(|next| {
+            let res = previous + next;
+            rotations += res.abs() / 100 + i64::from(previous != 0 && res <= 0);
+            previous = (res % 100 + 100) % 100;
+        });
+    println!("Result for the day is: {}", rotations);
+}
