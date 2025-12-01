b451c86be89ba05385a77a0b8199b01e89731ea0
Swarnim Arun <swarnimarun11@gmail.com>
2025-12-02 00:43:03 +0530

    feat: day 1 part 1 solved!
    I made an error when calculating the numbers so got stuck for like 30 minutes on it.

--- aoc_rust/src/day1/part1.rs
+++ aoc_rust/src/day1/part1.rs
@@ -1,1 +1,20 @@
-pub fn run(_input: String) {}
+pub fn run(input: String) {
+    let mut result = 0;
+    _ = input
+        .lines()
+        .map(|s| {
+            if s.as_bytes()[0] == b'L' {
+                s[1..].parse::<i64>().unwrap() * -1
+            } else {
+                s[1..].parse::<i64>().unwrap()
+            }
+        })
+        .fold(50i64, |acc, res| {
+            let r = ((acc + res) % 100 + 100) % 100;
+            if r == 0 {
+                result += 1;
+            }
+            r
+        });
+    println!("Result for the day is: {}", result);
+}
